53585969b78ff2e23806c6c678d8194773c85441
KIPRI <hi@kipri.com>
2024-02-05 22:06:55 +0200

    feat: build process 2.0

--- build.ts
+++ build.ts
@@ -1,8 +1,16 @@
-const fileText = await Bun.file("./package.json").text();
-const packageJson = JSON.parse(fileText);
+import { spawn, spawnSync, build, file, write, env } from "bun";
+
+const PACKAGE_JSON = file("./package.json");
+const DOT_ENV = file("./.env.local");
+const NODE_ENV = env["NODE_ENV"] || "development";
+
+const isProduction = NODE_ENV === "production";
+const mv = isProduction ? "mv" : "cp";
+
+const packageJson = JSON.parse(await PACKAGE_JSON.text());
 const dependencies = Object.keys(packageJson.dependencies);
 
-await Bun.build({
+await build({
     root: "./",
     entrypoints: ["./src/index.ts"],
     outdir: "./dist",
@@ -12,10 +20,16 @@ await Bun.build({
     external: dependencies,
 });
 
-// TODO
-// copy package.json to dist
-// copy node_modules to dist
-// copy .env to dist
-// copy static dir to dist
+await write(file("./dist/package.json"), await PACKAGE_JSON.arrayBuffer());
+await write(file("./dist/.env"), await DOT_ENV.arrayBuffer());
+
+spawnSync(["bun", "tw"]);
+
+spawn({ cmd: [mv, "-r", "./static", "./dist/static"] });
+if (isProduction) {
+    spawn({ cmd: [mv, "-r", "./node_modules", "./dist/node_modules"] });
+} else {
+    spawn(["bun", "install"], { cwd: "./dist" });
+}
 
 export {};
