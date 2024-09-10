bfa68f6c5dba9b45083e2e8c033e715dd9e5164c
KIPRI <hi@kipri.dev>
2024-09-10 22:25:08 +0300

    chore: with auth

--- src/auth/lib/with-auth.ts
+++ src/auth/lib/with-auth.ts
@@ -1,35 +1,23 @@
-import { type Logger } from "@bogeychan/elysia-logger/src/types";
-import { type Cookie as ElysiaCookie } from "elysia";
-import { type Cookie, type Session, type User } from "lucia";
+import { type InferContext } from "elysia";
+import { type Session, type User } from "lucia";
 
 import { auth } from "@/auth";
 import { isCSRF } from "@/auth/lib/csrf";
+import { type App } from "@/index";
 
-type AuthInfoContext = {
-    request: Request;
-    body: unknown;
-    query: Record<string, string | undefined>;
-    cookie: Record<string, ElysiaCookie<string | undefined>>;
-    log: Logger;
-};
-
-export async function withAuth(ctx: AuthInfoContext) {
-    let user: User | null = null;
-    let session: Session | null = null;
-
+export async function withAuth(
+    ctx: InferContext<App>,
+): Promise<{ user: User | null; session: Session | null }> {
     if (ctx.request.method !== "GET" && isCSRF(ctx.request.headers)) {
-        const headersEntries = ctx.request.headers.entries();
-        const headers = Array.from(headersEntries).reduce(
-            function createHeadersObj(acc, [key, value]) {
-                acc[key] = value;
-                return acc;
-            },
-            {} as Record<string, string>,
-        );
-
         ctx.log.warn(
             {
-                headers: JSON.stringify(headers),
+                headers: JSON.stringify(
+                    Array.from(ctx.request.headers.entries()).reduce(
+                        (acc, [key, value]) =>
+                            Object.assign(acc, { [key]: value }),
+                        {} as Record<string, string>,
+                    ),
+                ),
                 body: JSON.stringify(ctx.body),
                 cookies: JSON.stringify(ctx.request.headers.getSetCookie()),
                 query: JSON.stringify(ctx.query),
@@ -37,7 +25,10 @@ export async function withAuth(ctx: AuthInfoContext) {
             "CSRF detected!",
         );
 
-        return { user, session };
+        return {
+            user: null,
+            session: null,
+        };
     }
 
     const cookieHeader = ctx.request.headers.get("Cookie");
@@ -46,7 +37,10 @@ export async function withAuth(ctx: AuthInfoContext) {
     if (!cookieHeader && !authorizationHeader) {
         ctx.log.trace("No session cookie or bearer token found.");
 
-        return { user, session };
+        return {
+            user: null,
+            session: null,
+        };
     }
 
     const sessionId = cookieHeader
@@ -59,35 +53,40 @@ export async function withAuth(ctx: AuthInfoContext) {
             "Could not read session id.",
         );
 
-        return { user, session };
+        return {
+            user: null,
+            session: null,
+        };
     }
 
-    ({ user, session } = await auth.validateSession(sessionId));
+    const { user, session } = await auth.validateSession(sessionId);
     ctx.log.trace({ userId: user?.id, sessionId }, "Session found.");
 
     if (!cookieHeader) {
-        return { user, session };
-    }
-
-    let sessionCookie: Cookie;
-
-    if (session && session.fresh) {
-        ctx.log.trace({ sessionId }, "Session cookie refreshed.");
-        sessionCookie = auth.createSessionCookie(session.id);
-        ctx.cookie[sessionCookie.name].set({
-            ...sessionCookie.attributes,
-            value: sessionCookie.value,
-        });
+        return {
+            user,
+            session,
+        };
     }
 
     if (!session) {
         ctx.log.trace({ sessionId }, "Session expired.");
-        sessionCookie = auth.createBlankSessionCookie();
-        ctx.cookie[sessionCookie.name].set({
-            ...sessionCookie.attributes,
-            value: sessionCookie.value,
+        const cookie = auth.createBlankSessionCookie();
+        ctx.cookie[cookie.name].set({
+            ...cookie.attributes,
+            value: cookie.value,
+        });
+    } else if (session.fresh) {
+        ctx.log.trace({ sessionId }, "Session cookie refreshed.");
+        const cookie = auth.createSessionCookie(session.id);
+        ctx.cookie[cookie.name].set({
+            ...cookie.attributes,
+            value: cookie.value,
         });
     }
 
-    return { user, session };
+    return {
+        user,
+        session,
+    };
 }
