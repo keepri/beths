02ad721380834e47bee3a8e23b4e46913fb4d05c
KIPRI <hi@kipri.dev>
2024-09-16 21:42:19 +0300

    chore: misc

--- src/routes/pages.ts
+++ src/routes/pages.ts
@@ -3,13 +3,13 @@ import { type Elysia } from "elysia";
 import { PageController } from "@/controllers/page/controller";
 
 const NAME = "Pages";
-const PREFIX = "/";
+const PREFIX = "";
 
 export function pagesRoutes(app: Elysia) {
     return app.group(PREFIX, function handleRoutes(group) {
         group.config.name = NAME;
 
-        group.get("", PageController.home);
+        group.get("/", PageController.home);
 
         return group;
     });
